7fe5eea95e2f7ab8968038375589a3d6b92122e7
Sadamori Kojaku <skojaku@iu.edu>
2024-01-20 23:35:58 -0500

    add helios web

---
+++ vis/helios-web/src/helios.js
@@ -0,0 +1,8 @@
+import { Helios} from "./core/HeliosCore.js";
+import * as xnet from "./utilities/parsers/xnet.js";
+import * as gexf from "./utilities/parsers/gexf.js";
+import * as gml from "./utilities/parsers/gml.js";
+import { BehaviorFilter} from "./behaviors/BehaviorFilter.js";
+
+
+export { Helios, xnet, gml, gexf, BehaviorFilter};
